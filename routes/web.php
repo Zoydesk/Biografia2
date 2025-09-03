1af4c773fefc8b511cce2c21d8342399ef6d62ae
Raul Beltran <raulb2753@gmail.com>
2025-09-03 16:42:44 -0500

    escuela

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/Yo', function () {
     return view('yo');
 });
+
+Route::get('/academia', function () {
+    return view('academia'); 
+})->name('academia');
